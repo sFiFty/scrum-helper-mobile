6f250fbef9466e3aff702b68bad37456c9169708
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-03-20 09:00:50 +0200

    added auth through the firebase

--- app/screens/login.js
+++ app/screens/login.js
@@ -1,19 +1,26 @@
 import Exponent from 'expo'
 import React, {Component} from 'react'
+import firebase from 'firebase'
 import {View, StyleSheet} from 'react-native'
 import FacebookButton from '../components/facebookButton'
 
 export default class Login extends Component {
 
+  authenticate = token => {
+    const provider = firebase.auth.FacebookAuthProvider
+    const credential = provider.credential(token)
+    return firebase.auth().signInWithCredential(credential)
+  }
+
   login = async () => {
     const ADD_ID = '960367194110449'
     const options = {
-      permissions: ['public_profile']
+      permissions: ['public_profile', 'email']
     }
     const {type, token} = await Exponent.Facebook.logInWithReadPermissionsAsync(ADD_ID, options)
     if (type === 'success') {
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
-      console.log(await response.json())
+      this.authenticate(token)
     }
   }
 
