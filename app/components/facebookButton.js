b5a88820a2216f3446afd8581dd46a3de75ec9e5
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-03-20 08:16:55 +0200

    stylized facebookButton

--- app/components/facebookButton.js
+++ app/components/facebookButton.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react'
-import {TouchableHighlight, Text, StyleSheet} from 'react-native'
+import {TouchableHighlight, Text, StyleSheet, View} from 'react-native'
+import {FontAwesome} from '@expo/vector-icons'
 
 export default class FacebookButton extends Component {
-
   render() {
     return (
-      <TouchableHighlight>
-        <Text>Login with Facebook</Text>
+      <TouchableHighlight style={styles.button} onPress={this.props.onPress}>
+        <View style={styles.buttonContainer}>
+          <FontAwesome name={'facebook-f'} size={20} color={'white'} />
+          <Text style={styles.buttonText}>Login with Facebook</Text>
+        </View>      
       </TouchableHighlight>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    width: 220,
+    backgroundColor: '#3b5998',
+    borderRadius: 50
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    marginLeft: 15
+  }
+})
--- app/screens/login.js
+++ app/screens/login.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
+import {View, StyleSheet} from 'react-native'
+import FacebookButton from '../components/facebookButton'
 
 export default class Login extends Component {
 
   render() {
     return (
-      <View />
+      <View style={styles.container} >
+        <FacebookButton onPress={() => console.log('press')}/>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
